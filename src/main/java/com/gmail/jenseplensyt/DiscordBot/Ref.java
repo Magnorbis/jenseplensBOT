43b94394ea72c0a5ab662558a5db8ce1034ad968
jenseplens <jens.horsthuis@gmail.com>
2018-03-02 15:38:55 +0100

    Update Ref.java

--- src/main/java/com/gmail/jenseplensyt/DiscordBot/Ref.java
+++ src/main/java/com/gmail/jenseplensyt/DiscordBot/Ref.java
@@ -1,6 +1,6 @@
 package com.gmail.jenseplensyt.DiscordBot;
 
 public class Ref {
-	public static final String token = "NDE5MTIyNTU5MjgwMjE4MTEy.DXrh1Q.1RbiLiGOFxPa5rbRZanxHeVQY3E";
+	public static final String token = "token";
 	public static final String prefix = ">";
 }
